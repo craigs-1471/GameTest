37aa2982cbeedc521876f194387fbe607165b256
Spencer Craig <40411621@live.napier.ac.uk>
2019-03-26 14:49:29 +0000

    First Commit

---
+++ src/data/Boot.java
@@ -0,0 +1,23 @@
+package data;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class Boot {
+	
+	public Boot() {
+		Display.setTitle("Spencer's Game");
+		try {
+			Display.setDisplayMode(new DisplayMode(600, 400));
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new Boot();
+	}
+	
+}
